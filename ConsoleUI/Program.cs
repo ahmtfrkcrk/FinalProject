299fa69b11f17d4327403fb1a813a6593eab5352
FARUK <FARUK@FARUKPC>
2023-05-04 14:26:30 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        //S"O"LID
+        //Open Closed Principle 
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var product in productManager.GetAllByCategoryId(3))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+            foreach (var product2 in productManager.GetByUnitPrice(18,21))
+            {
+                Console.WriteLine("{0}======{1}",product2.ProductName,product2.UnitPrice);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context : Db tabloları ile proje classlarını bağlamak 
+    //Dbsete bağladığımız tablolar bizim classımıza denk gelmesi gerekiyor.
+    class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=FARUKPC\SQL2019;Database=northwnd;Trusted_Connection=true");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
